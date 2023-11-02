6947f11cfd1f893fe8dde8006d70c12c492e7fb1
墨格 <118667307+hereismg@users.noreply.github.com>
2023-11-02 21:14:47 +0800

    钢条切割问题新增测试用例

--- Chapter15/Steel_bar_cutting/src/solution.cpp
+++ Chapter15/Steel_bar_cutting/src/solution.cpp
@@ -8,11 +8,22 @@ public:
     int priceTable[LEN]{0};
     int l;
     int n;
+
+    int maxPrice[LEN];
     Solution(){
         cin >> l >> n;
-        for (int i=0; i<n; i++) cin >> priceTable[i];
+        for (int i=1; i<=n; i++) cin >> priceTable[i];
 
-        
+        for (int currentLen=1; currentLen<=l; currentLen++){
+            int max = 0;
+            for (int cut=1; cut<=n && cut<=currentLen; cut++){
+                // dp[cut][currentLen] = dp[n+1][currentLen - cut] + priceTable[cut];
+                int temp = maxPrice[currentLen - cut] + priceTable[cut];
+                max = temp > max ? temp : max;
+            }
+            maxPrice[currentLen] = max;
+        }
+        cout << maxPrice[l] << endl;
     }
 };
 
