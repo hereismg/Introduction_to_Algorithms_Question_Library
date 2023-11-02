73542c50984f882d84eaaa46ae9dd607ed22025f
墨格 <118667307+hereismg@users.noreply.github.com>
2023-11-02 20:54:23 +0800

    存档

--- Chapter15/Steel_bar_cutting/src/solution.cpp
+++ Chapter15/Steel_bar_cutting/src/solution.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+const int LEN = 1e5;
+
 class Solution{
 public:
+    int priceTable[LEN]{0};
+    int l;
+    int n;
     Solution(){
+        cin >> l >> n;
+        for (int i=0; i<n; i++) cin >> priceTable[i];
+
         
     }
 };
