179691e1aed3a4855a00bd0e204e8c4e044ae1ef
墨格 <118667307+hereismg@users.noreply.github.com>
2023-11-04 16:14:03 +0800

    矩阵链乘法给出标准程序

--- Chapter15/Matrix-chain multiplication/src/solution.cpp
+++ Chapter15/Matrix-chain multiplication/src/solution.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 const int LEN = 1e5;
@@ -6,14 +7,15 @@ const int LEN = 1e5;
 class Solution{
 public:
     int sides[LEN]{0};
-    int dp[100][100]{0};
+    vector<vector<int>> dp;
     int n;
-    Solution(){
+    Solution(): dp(100, vector<int>(100, 0)){
         cin >> n;
         for (int i=0; i<=n; i++) cin >> sides[i];
 
         int result = m(1, n);
         cout << result << endl;
+        // debug();
     }
 
     int m(int l, int r){
@@ -21,12 +23,22 @@ public:
             return dp[l][r];
         }else{
             int min = 9999999;
-            for (int i=l; i<=r; i++){
-                int temp = m(l, i) + m(i+1, r) + sides[i-1] * sides[i] * sides[i+1];
+            for (int i=l; i<=r-1; i++){
+                int leftSum = m(l, i), rightSum = m(i+1, r);
+                int temp = leftSum + rightSum + sides[l-1] * sides[i] * sides[r];
                 min = temp < min ? temp : min;
             }
             dp[l][r] = min;
             return dp[l][r];
+        }
+    }
+
+    void debug(){
+        for (int i=0; i<=n; i++){
+            for (int j=0; j<=n; j++){
+                cout << dp[i][j] << "\t\t";
+            }
+            cout << endl;
         }
     }
 };
